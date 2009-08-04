849ab31aebbbe60fc8c8096958bc30affa568c2a
Jared Grippe <jared@deadlyicon.com>
2009-08-04 09:59:15 -0700

    refactored Object.Event into a Class mixin called usage var Frog = Class.create(Observable, { .. });

--- src/livepipe.js
+++ src/livepipe.js
@@ -18,84 +18,102 @@ var $value = function(value){
     return typeof(value) == 'function' ? value() : value;
 };
 
-Object.Event = {
-    extend: function(object){
-        object._objectEventSetup = function(event_name){
-            this._observers = this._observers || {};
-            this._observers[event_name] = this._observers[event_name] || [];
-        };
-        object.observe = function(event_name,observer){
-            if(typeof(event_name) == 'string' && typeof(observer) != 'undefined'){
-                this._objectEventSetup(event_name);
-                if(!this._observers[event_name].include(observer))
-                    this._observers[event_name].push(observer);
-            }else
-                for(var e in event_name)
-                    this.observe(e,event_name[e]);
-        };
-        object.stopObserving = function(event_name,observer){
-            this._objectEventSetup(event_name);
-            if(event_name && observer)
-                this._observers[event_name] = this._observers[event_name].without(observer);
-            else if(event_name)
-                this._observers[event_name] = [];
-            else
-                this._observers = {};
-        };
-        object.observeOnce = function(event_name,outer_observer){
-            var inner_observer = function(){
-                outer_observer.apply(this,arguments);
-                this.stopObserving(event_name,inner_observer);
-            }.bind(this);
-            this._objectEventSetup(event_name);
-            this._observers[event_name].push(inner_observer);
-        };
-        object.notify = function(event_name){
-            this._objectEventSetup(event_name);
-            var collected_return_values = [];
-            var args = $A(arguments).slice(1);
-            try{
-                for(var i = 0; i < this._observers[event_name].length; ++i)
-                    collected_return_values.push(this._observers[event_name][i].apply(this._observers[event_name][i],args) || null);
-            }catch(e){
-                if(e == $break)
-                    return false;
-                else
-                    throw e;
-            }
-            return collected_return_values;
-        };
-        if(object.prototype){
-            object.prototype._objectEventSetup = object._objectEventSetup;
-            object.prototype.observe = object.observe;
-            object.prototype.stopObserving = object.stopObserving;
-            object.prototype.observeOnce = object.observeOnce;
-            object.prototype.notify = function(event_name){
-                if(object.notify){
-                    var args = $A(arguments).slice(1);
-                    args.unshift(this);
-                    args.unshift(event_name);
-                    object.notify.apply(object,args);
-                }
-                this._objectEventSetup(event_name);
-                var args = $A(arguments).slice(1);
-                var collected_return_values = [];
-                try{
-                    if(this.options && this.options[event_name] && typeof(this.options[event_name]) == 'function')
-                        collected_return_values.push(this.options[event_name].apply(this,args) || null);
-                    for(var i = 0; i < this._observers[event_name].length; ++i)
-                        collected_return_values.push(this._observers[event_name][i].apply(this._observers[event_name][i],args) || null);
-                }catch(e){
-                    if(e == $break)
-                        return false;
-                    else
-                        throw e;
-                }
-                return collected_return_values;
-            };
-        }
-    }
-};
+
+var Observable = (function() {
+
+  function objectEventSetup(event_name){
+      this._observers = this._observers || {};
+      this._observers[event_name] = this._observers[event_name] || [];
+  }
+  
+  function observe(event_name,observer){
+      if(typeof(event_name) == 'string' && typeof(observer) != 'undefined'){
+          objectEventSetup(event_name);
+          if(!this._observers[event_name].include(observer))
+              this._observers[event_name].push(observer);
+      }else
+          for(var e in event_name)
+              this.observe(e,event_name[e]);
+  }
+  
+  function stopObserving(event_name,observer){
+      objectEventSetup(event_name);
+      if(event_name && observer)
+          this._observers[event_name] = this._observers[event_name].without(observer);
+      else if(event_name)
+          this._observers[event_name] = [];
+      else
+          this._observers = {};
+  }
+  
+  function observeOnce(event_name,outer_observer){
+      var inner_observer = function(){
+          outer_observer.apply(this,arguments);
+          this.stopObserving(event_name,inner_observer);
+      }.bind(this);
+      objectEventSetup(event_name);
+      this._observers[event_name].push(inner_observer);
+  }
+  
+  function notify(event_name){
+      objectEventSetup(event_name);
+      var collected_return_values = [];
+      var args = $A(arguments).slice(1);
+      try{
+          for(var i = 0; i < this._observers[event_name].length; ++i)
+              collected_return_values.push(this._observers[event_name][i].apply(this._observers[event_name][i],args) || null);
+      }catch(e){
+          if(e == $break)
+              return false;
+          else
+              throw e;
+      }
+      return collected_return_values;
+  }
+  
+  function instanceNotify(event_name){
+      if(this.constructor && this.constructor.notify){
+          var args = $A(arguments).slice(1);
+          args.unshift(this);
+          args.unshift(event_name);
+          this.constructor.notify.apply(this.constructor,args);
+      }
+      objectEventSetup(event_name);
+      var args = $A(arguments).slice(1);
+      var collected_return_values = [];
+      try{
+          if(this.options && this.options[event_name] && typeof(this.options[event_name]) == 'function')
+              collected_return_values.push(this.options[event_name].apply(this,args) || null);
+          for(var i = 0; i < this._observers[event_name].length; ++i)
+              collected_return_values.push(this._observers[event_name][i].apply(this._observers[event_name][i],args) || null);
+      }catch(e){
+          if(e == $break)
+              return false;
+          else
+              throw e;
+      }
+      return collected_return_values;
+  };
+    
+  Object.Event = {
+      extend: function(object){
+          Object.extend(object, Observable);
+          object.prototype.notify = notify;
+          if (object.prototype) Object.extend(object.prototype, Observable);
+      }
+  };
+  
+  return {
+    observe:       observe,
+    stopObserving: stopObserving,
+    observeOnce:   observeOnce,
+    notify:        instanceNotify
+  };
+  
+})();
+
+
+
 
 /* Begin Core Extensions */
 
